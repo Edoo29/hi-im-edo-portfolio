c270667629c4b69f625d7be731828a9498f8cea7
Edo <edoardo.zaffaroni@gmail.com>
2024-07-17 14:53:55 +0200

    Added the projects section

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,14 @@
+import { dancing_script } from "../font";
+
 export default function Projects() {
   return (
-    <div
-      id="skills"
-      className="hidden md:block mt-[700px] h-10 md:h-[200px]"
-    ></div>
+    <div id="skills" className="hidden md:block mt-[700px] h-[200px]">
+      <p
+        className={`${dancing_script.className} text-white text-center text-5xl`}
+      >
+        Projects
+      </p>
+      <p className="text-white text-center text-xl mt-10">No projects yet.</p>
+    </div>
   );
 }
